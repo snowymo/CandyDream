193e35a4b7d4c2f2c77da691f190fa717251c2cc
Zhenyi He <snowymohehe@gmail.com>
2017-09-02 23:42:41 -0400

    add animation, adjust scale and rotation, test order of vive tracker

---
+++ Assets/Scripts/DemonAnimatorCtrl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemonAnimatorCtrl : MonoBehaviour {
+
+    Vector3 lastPos, curPos;
+
+    float dis;
+
+    public float speed;
+
+    public float speedThres;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        curPos = GetComponent<ApplyTheme>().tracked_transform.position;
+        dis = Vector3.Distance(curPos, lastPos);
+        speed = dis / Time.deltaTime;
+        
+            GetComponent<ApplyTheme>().curObj.GetComponent<Animator>().SetBool("Walk", (speed > speedThres));
+        
+        
+        lastPos = curPos;
+
+    }
+}
